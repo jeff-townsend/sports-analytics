0cd422119f428daef3ddd55227e5d7febd5281a8
jeff-townsend <106111263+jeff-townsend@users.noreply.github.com>
2023-02-15 11:49:56 -0500

    Update [NFL] Team Elo.R

--- [NFL] Team Elo.R
+++ [NFL] Team Elo.R
@@ -2,36 +2,66 @@ library(tidyverse)
 library(nflreadr)
 
 # set up the data
+starting.season <- 2012
 nfl.pbp <-
-  load_pbp(seasons = c(2020:2022)) %>%
+  load_pbp(seasons = c(starting.season:2022)) %>%
   filter(game_seconds_remaining == 3600)
 
+# initiate tables/variables for loop
+nfl.elo <- data.frame(team = character(),
+                      season = integer(),
+                      week = integer(),
+                      pre_elo = double(),
+                      post_elo = double(),
+                      elo_delta = double())
+nfl.results <- data.frame(game_id = character(),
+                          week = integer(),
+                          season = integer(),
+                          home_win = double(),
+                          home_pre_elo = double(),
+                          away_pre_elo = double(),
+                          home_expectation = double(),
+                          home_elo_delta = double(),
+                          away_elo_delta = double(),
+                          spread_log_loss = double(),
+                          elo_log_loss = double())
+
+pyth.exp <- 3.25
+k.factor <- 48
+hfa <- 42
+yoy.reg <- 0.55
+
 # run season-level loop
-pyth.exp <- 2.37
-k.factor <- 64
-hfa <- 20
-s <- 2020
-for(s in 2020:2022)
+s <- starting.season
+for(s in starting.season:2022)
   {
     nfl.games <-
       nfl.pbp %>%
       filter(season == s) %>%
-      distinct(game_id, week, home_team, away_team, home_score, away_score, spread_line, result, location) %>%
+      distinct(game_id, week, home_team, away_team, home_score, away_score, spread_line, total_line, result, location) %>%
       mutate(home_win = ifelse(result > 0, 1, ifelse(result < 0, 0, 0.5)),
              away_win = 1 - home_win,
+             home_implied_score = total_line/2 + spread_line/2,
+             away_implied_score = total_line/2 - spread_line/2,
+             home_implied_pct = home_implied_score^pyth.exp / (home_implied_score^pyth.exp + away_implied_score^pyth.exp),
+             away_implied_pct = 1 - home_implied_pct,
              home_pyth = home_score^pyth.exp / (home_score^pyth.exp + away_score^pyth.exp),
              is_neutral = ifelse(location == "Neutral", 1, 0))
     
     # initiate variables/tables for loop
-    nfl.elo <-
+    nfl.starting.elo <-
       rbind(nfl.games %>% filter(week == 1) %>% select(home_team, week) %>% rename(team = home_team),
             nfl.games %>% filter(week == 1) %>% select(away_team, week) %>% rename(team = away_team)) %>%
-      mutate(pre_elo = 1500,
+      left_join(nfl.elo %>%
+                  filter(week == 23,
+                         season == s - 1) %>%
+                  select(team, season, pre_elo),
+                by = "team") %>%
+      mutate(season = s,
+             pre_elo = ifelse(is.na(pre_elo), 1500, (pre_elo-1500)*yoy.reg + 1500),
+             post_elo = pre_elo,
              elo_delta = 0)
-    nfl.results <- data.frame(matrix(ncol = 10, nrow = 0))
-    colnames(nfl.results) <- c("game_id", "week", "season", "home_win",
-                               "home_pre_elo", "away_pre_elo", "home_expectation",
-                               "home_elo_delta", "away_elo_delta", "log_loss")
+    nfl.elo <- rbind(nfl.elo, nfl.starting.elo)
     
     
     # run week-level loop
@@ -40,17 +70,19 @@ for(s in 2020:2022)
       {
         nfl.pre.elo <-
           nfl.elo %>%
-          filter(week == w)
+          filter(week == w,
+                 season == s)
         
         nfl.weekly.games <-
           nfl.games %>%
-          inner_join(nfl.pre.elo, by = c("home_team" = "team", "week", "season")) %>%
+          inner_join(nfl.pre.elo, by = c("home_team" = "team", "week")) %>%
           rename(home_pre_elo = pre_elo) %>%
-          inner_join(nfl.pre.elo, by = c("away_team" = "team", "week", "season")) %>%
+          inner_join(nfl.pre.elo, by = c("away_team" = "team", "week")) %>%
           rename(away_pre_elo = pre_elo) %>%
           mutate(home_expectation = 1 / (1 + 10 ^ ((away_pre_elo - (home_pre_elo + (1-is_neutral)*hfa))/400)),
                  away_expectation = 1 / (1 + 10 ^ (((home_pre_elo + (1-is_neutral)*hfa) - away_pre_elo)/400))) %>%
-          mutate(log_loss = -(home_win*log(home_expectation) + away_win*log(away_expectation))) %>%
+          mutate(spread_log_loss = -(home_win*log(home_implied_pct) + away_win*log(away_implied_pct)),
+                 elo_log_loss = -(home_win*log(home_expectation) + away_win*log(away_expectation))) %>%
           mutate(home_elo_delta = k.factor * (home_win - home_expectation),
                  away_elo_delta = k.factor * (away_win - away_expectation),
                  home_post_elo = home_pre_elo + home_elo_delta,
@@ -59,26 +91,41 @@ for(s in 2020:2022)
         nfl.post.elo <-
           nfl.pre.elo %>%
           left_join(nfl.weekly.games %>%
-                      select(home_team, week, season, home_post_elo),
-                    by = c("team" = "home_team", "week", "season")) %>%
+                      select(home_team, week, home_post_elo),
+                    by = c("team" = "home_team", "week")) %>%
           left_join(nfl.weekly.games %>%
-                      select(away_team, week, season, away_post_elo),
-                    by = c("team" = "away_team", "week", "season")) %>%
-          mutate(post_elo = ifelse(!is.na(home_post_elo), home_post_elo,
+                      select(away_team, week, away_post_elo),
+                    by = c("team" = "away_team", "week")) %>%
+          mutate(season = s,
+                 post_elo = ifelse(!is.na(home_post_elo), home_post_elo,
                                    ifelse(!is.na(away_post_elo), away_post_elo,
-                                          pre_elo)),
+                                          pre_elo))) %>%
+          select(team, week, season, pre_elo, post_elo)
+        
+        # update post_elo and elo_delta from current week
+        nfl.elo <-
+          nfl.elo %>%
+          left_join(nfl.post.elo, by = c("team", "week", "season")) %>%
+          rename(pre_elo = pre_elo.x,
+                 post_elo = post_elo.x) %>%
+          mutate(post_elo = ifelse(is.na(post_elo.y), post_elo, post_elo.y),
                  elo_delta = post_elo - pre_elo) %>%
           select(team, week, season, pre_elo, post_elo, elo_delta)
         
+        # insert pre_elo for the following week
         nfl.elo <- rbind(nfl.elo,
                          nfl.post.elo %>%
-                           select(team, week, season, post_elo, elo_delta) %>%
-                           mutate(week = week + 1) %>%
-                           rename(pre_elo = post_elo))
+                           select(team, week, season, post_elo) %>%
+                           mutate(week = week + 1) %>% 
+                           rename(pre_elo = post_elo) %>%
+                           mutate(post_elo = pre_elo, # initiate post_elo as pre_elo, which we'll update the subsequent week
+                                  elo_delta = 0)) # initiate elo_delta as 0, which we'll update the subsequent week
+        
         nfl.results <- rbind(nfl.results,
                              nfl.weekly.games %>%
+                               mutate(season = s) %>%
                                select(game_id, week, season, home_win, home_pre_elo, away_pre_elo,
-                                      home_expectation, home_elo_delta, away_elo_delta, log_loss))
+                                      home_expectation, home_elo_delta, away_elo_delta, spread_log_loss, elo_log_loss))
         
         w <- w + 1
     }
@@ -98,7 +145,10 @@ nfl.final.elo <-
   arrange(desc(final_elo))
 
 # check for prediction accuracy
-mean(nfl.results$log_loss)
+mean(nfl.results$spread_log_loss)
+nfl.results %>%
+  filter(season != starting.season) %>%
+  summarize(log_loss = mean(elo_log_loss))
 # check for home-field advantage (should be 0 once controlled for)
 mean(nfl.results$home_elo_delta)
 # check for prior-year effects
