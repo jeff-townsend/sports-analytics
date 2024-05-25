4427f6f08e17c250d41687dc6cbe5cb525c4bf3e
jeff-townsend <106111263+jeff-townsend@users.noreply.github.com>
2024-05-25 18:20:25 -0400

    Create [NBA] Basketball Reference Scraping.R

---
+++ [NBA] Basketball Reference Scraping.R
@@ -0,0 +1,61 @@
+library(rvest)
+library(tidyverse)
+library(lubridate)
+
+setwd("/Users/jtownsend/Downloads")
+
+# initialize data frame
+
+season <- 2024
+season.df <- data.frame(matrix(ncol = 13, nrow = 0))
+col.names <- c("game_id", "season", "game_date", "start_time", "away_team", "away_points",
+               "home_team", "home_points", "box_score", "overtimes", "attendance", "arena", "notes")
+colnames(season.df) <- col.names
+
+### loop through each month
+
+m <- 1
+
+for(m in 1:12)
+{
+  
+  month <- tolower(month.name[m])
+  url <- paste0("https://www.basketball-reference.com/leagues/NBA_", season, "_games-", month, ".html")
+  
+  webpage <- NULL
+  tryCatch(webpage <- read_html(url),
+           error = function(e){'empty page'})
+  
+  if(is.null(webpage) == FALSE)
+  {
+    
+    dates <-
+      webpage %>% 
+      html_nodes("table#schedule > tbody > tr > th") %>% 
+      html_text()
+    
+    game.id <-
+      webpage %>% 
+      html_nodes("table#schedule > tbody > tr > th") %>%
+      html_attr("csk")
+    
+    data <-
+      webpage %>% 
+      html_nodes("table#schedule > tbody > tr > td") %>% 
+      html_text() %>%
+      matrix(ncol = length(col.names) - 3, byrow = TRUE)
+    
+    month.df <- as.data.frame(cbind(game.id, season, dates, data), stringsAsFactors = FALSE)
+    colnames(month.df) <- col.names
+    
+    season.df <- rbind(season.df, month.df)
+    
+  }
+  
+  m <- m + 1
+  
+}
+
+season.df <- season.df %>% arrange(game_id)
+season.df$game_date <- mdy(season.df$game_date)
+write.csv(season.df, paste0("basketball_reference_games_", season, ".csv"))
