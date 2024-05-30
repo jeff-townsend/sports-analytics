a36c66d5dd1f63baa28af21bb46e5321da98991a
jeff-townsend <106111263+jeff-townsend@users.noreply.github.com>
2024-05-30 17:51:29 -0400

    Update [NBA] NBA Predictive Modeling.R

--- [NBA] NBA Predictive Modeling.R
+++ [NBA] NBA Predictive Modeling.R
@@ -1,16 +1,17 @@
 library(readr)
 library(tidyverse)
 library(lubridate)
+library(ggthemes)
 
 setwd("/Users/jtownsend/Downloads")
+
+starting.season <- 2010
+ending.season <- 2024
 
 bref.df <- data.frame(matrix(ncol = 13, nrow = 0))
 col.names <- c("game_id", "season", "game_date", "start_time", "away_team", "away_points",
                "home_team", "home_points", "box_score", "overtimes", "attendance", "arena", "notes")
 colnames(bref.df) <- col.names
-
-starting.season <- 2022
-ending.season <- 2024
 
 s <- starting.season
 
@@ -29,9 +30,10 @@ for(s in starting.season:ending.season)
 }
 
 # use game dates to determine whether it's a playoff game
-p <- c("2015-04-18", "2016-04-16", "2017-04-15", "2018-04-14", "2019-04-13",
+p <- c("2010-04-17", "2011-04-16", "2012-04-28", "2013-04-20", "2014-04-19",
+       "2015-04-18", "2016-04-16", "2017-04-15", "2018-04-14", "2019-04-13",
        "2020-08-17", "2021-05-22", "2022-04-16", "2023-04-15", "2024-04-20")
-playoff.dates <- data.frame(season = c(2015:2024),
+playoff.dates <- data.frame(season = c(2010:2024),
                             playoff_start_date = p)
 
 ## create new data frame where each observation is a team game
@@ -59,13 +61,16 @@ home.df <-
 merge.df <-
   rbind(away.df, home.df) %>%
   group_by(season, team) %>%
-  mutate(game_number = rank(game_date))
+  mutate(game_number = rank(game_date)) %>%
+  ungroup()
 
-games.tmp <-
+games.df <-
   merge.df %>%
   arrange(game_id) %>%
+  # add game results
   mutate(pd = points - points_allowed,
          is_win = ifelse(pd > 0, 1, 0)) %>%
+  # add game type
   inner_join(playoff.dates, by = "season") %>%
   mutate(game_type = ifelse(game_date >= playoff_start_date | 
                               notes == "Play-In Game" | 
@@ -74,102 +79,63 @@ games.tmp <-
          game_subtype = ifelse(notes == "Play-In Game", "Play-In",
                                ifelse(notes == "In-Season Tournament", "In-Season Tournament", game_type))) %>%
   select(-playoff_start_date) %>%
+  # add regular season game number
+  group_by(season, team, game_type) %>%
+  mutate(season_game_number = rank(game_number)) %>%
+  ungroup() %>%
+  # add days rest
   left_join(merge.df %>%
               mutate(game_number_match = game_number + 1) %>%
               select(season, team, game_date, game_number_match),
             by = c("season", "team", c("game_number" = "game_number_match")),
             suffix = c("", "_prior")) %>%
   mutate(days_rest = as.numeric(game_date - game_date_prior) - 1) %>%
-  select(-game_date_prior)
+  select(-game_date_prior) %>%
+  # add season-to-date point differential
+  group_by(season, team) %>%
+  mutate(pre_cum_pd = ifelse(game_number == 1, 0, (cumsum(pd) - pd) / (game_number - 1))) %>%
+  ungroup()
 
-games.df <-
-  games.tmp %>%
-  inner_join(games.tmp %>%
-               select(game_id, season, team, days_rest),
+pred.games.df <-
+  games.df %>%
+  inner_join(games.df %>%
+               select(game_id, season, team, season_game_number, days_rest, pre_cum_pd),
              by = c("game_id", "season", c("opponent" = "team")),
              suffix = c("","_opp")) %>%
-  mutate(rest_diff = days_rest - days_rest_opp,
+  mutate(combined_game_number = (season_game_number + season_game_number_opp) / 2,
+         rest_diff = days_rest - days_rest_opp,
          days_rest_adj = ifelse(is.na(days_rest), 7, days_rest),
          days_rest_opp_adj = ifelse(is.na(days_rest_opp), 7, days_rest_opp),
          rest_diff_adj = days_rest_adj - days_rest_opp_adj,
          rest_diff_bucket = ifelse(rest_diff_adj > 2, 2,
-                                   ifelse(rest_diff_adj < -2, -2, rest_diff_adj))) %>%
+                                   ifelse(rest_diff_adj < -2, -2, rest_diff_adj)),
+         matchup_pd = pre_cum_pd - pre_cum_pd_opp) %>%
   select(-days_rest_adj, -days_rest_opp_adj, -rest_diff_adj)
 
-game.pred.mod <- lm(pd ~ is_home*game_type + rest_diff_bucket, data = games.df)
-summary(game.pred.mod)
+schedule.mod <- lm(pd ~ is_home*game_type + rest_diff_bucket, data = pred.games.df)
+summary(schedule.mod)
 
-games.df$pred_pd = predict(game.pred.mod, newdata = games.df)
-games.df$adj_pd = with(games.df, pd - pred_pd)
+pred.games.df$schedule_pd = predict(schedule.mod, newdata = pred.games.df)
+pred.games.df$adj_pd = with(pred.games.df, pd - schedule_pd)
 
-games.df %>%
-  group_by(game_type) %>%
-  summarize(error = mean(abs(adj_pd)))
-
-## summarize regular season data
-
-season.stats <-
-  games.df %>%
+matchup.df <-
+  pred.games.df %>%
   filter(game_type == "Regular Season") %>%
-  group_by(season, team) %>%
-  summarize(games = n(),
-            wins = sum(is_win),
-            losses = n() - sum(is_win),
-            win_rate = mean(is_win),
-            pf = mean(points),
-            pa = mean(points_allowed),
-            pd = mean(pd),
-            exp_pd = mean(pred_pd),
-            adj_pd = mean(adj_pd))
+  select(game_id, season, team, opponent, combined_game_number, pd, pre_cum_pd, pre_cum_pd_opp, matchup_pd, adj_pd) %>%
+  mutate(matchup_pd_error = abs(matchup_pd - adj_pd))
 
-xwinrate.mod <- lm(win_rate ~ pd, data = season.stats)
-#summary(xwinrate.mod)
+with(matchup.df, cor(matchup_pd, adj_pd)^2)
+ggplot(matchup.df, aes(x = matchup_pd, y = adj_pd)) +
+  geom_point()
 
-## split-half reliability
+matchup.agg <-
+  matchup.df %>%
+  group_by(combined_game_number) %>%
+  summarize(error = mean(matchup_pd_error),
+            n = n())
 
-rs.games.df <-
-  games.df %>%
-  filter(game_type == "Regular Season") %>%
-  select(-game_number) %>%
-  group_by(season, team) %>%
-  mutate(game_number = rank(game_date)) %>%
-  ungroup()
-
-odd.even.stats <-
-  rs.games.df %>%
-  mutate(is_odd = ifelse(game_number%%2 == 1, 1, 0)) %>%
-  group_by(season, team, is_odd) %>%
-  summarize(games = n(),
-            wins = sum(is_win),
-            losses = n() - sum(is_win),
-            win_rate = mean(is_win),
-            pf = mean(points),
-            pa = mean(points_allowed)) %>%
-  ungroup() %>%
-  mutate(pd = pf - pa,
-         xwin_rate = xwinrate.mod$coefficients[1] + xwinrate.mod$coefficients[2]*pd,
-         win_rate_delta = win_rate - xwin_rate)
-
-shr.data <-
-  odd.even.stats %>%
-  filter(is_odd == 1) %>%
-  select(-is_odd) %>%
-  inner_join(odd.even.stats %>%
-               filter(is_odd == 0) %>%
-               select(-is_odd),
-             by = c("season", "team"), suffix = c("_odd", "_even"))
-
-# ggplot(shr.data, aes(x = pd_even, y = win_rate_odd)) +
-#   geom_point() +
-#   geom_smooth(method = 'lm', formula = y ~ x)
-
-# shr.data %>%
-#   summarize(r = cor(win_rate_odd, win_rate_even)) %>%
-#   mutate(r2 = r^2)
-# 
-# shr.data %>%
-#   summarize(r = cor(pd_odd, pd_even)) %>%
-#   mutate(r2 = r^2)
-
-shr.mod <- lm(win_rate_even ~ pd_odd, data = shr.data)
-#summary(wr.mod)
+ggplot(matchup.agg, aes(x = combined_game_number, y = error)) +
+  geom_line() +
+  geom_smooth() +
+  theme_fivethirtyeight() +
+  ggtitle("Prediction error during an 82-game NBA season")
