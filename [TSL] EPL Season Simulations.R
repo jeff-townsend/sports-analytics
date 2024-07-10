d818a93c0343eeb4b3e201ec628b491fcc2e8703
jeff-townsend <106111263+jeff-townsend@users.noreply.github.com>
2024-07-10 15:24:07 -0400

    Update [TSL] EPL Season Simulations.R

--- [TSL] EPL Season Simulations.R
+++ [TSL] EPL Season Simulations.R
@@ -1,106 +1,103 @@
 library(tidyverse)
 library(ggthemes)
+library(readr)
 
-library(readxl)
-epl.schedule.load <- read_excel("Data Analysis/Team Super League/2023-24 EPL Schedule.xlsx", 
-                                col_types = c("numeric", "date", "text", "text", "numeric",
-                                              "numeric", "numeric", "numeric"))
-epl.team.strength <- read_excel("Data Analysis/Team Super League/2023-24 EPL Team Strength.xlsx")
-tsl.teams <- read_excel("Data Analysis/Team Super League/2023-24 Teams.xlsx")
-tsl.scoring <- data.frame(placement = c(1:20),
-                          tsl_points = c(80, 60, 40, 40, 25, 20, 16, 14, 12, 10,
-                                         8, 7, 6, 5, 4, 3, 2, 0, 0, 0))
+games.import <- read_csv("https://raw.githubusercontent.com/jeff-townsend/sports-analytics/main/data/EPL/epl_schedule_2425.csv",
+                         col_types = cols(date = col_date(format = "%m/%d/%y")))
 
-epl.schedule <-
-  epl.schedule.load %>%
-  mutate(completed = ifelse(week <= 3, completed, 0)) %>%
-  inner_join(epl.team.strength %>% select(team, win_rate), by = c("home" = "team")) %>%
-  rename(home_win_rate = win_rate) %>%
-  inner_join(epl.team.strength %>% select(team, win_rate), by = c("away" = "team")) %>%
-  rename(away_win_rate = win_rate) %>%
-  mutate(home_win_prob = ifelse(completed == 1, home_win,
-                                (home_win_rate - home_win_rate * away_win_rate) /
-                                  (home_win_rate + away_win_rate - 2 * home_win_rate * away_win_rate) * .78),
-         draw_prob = ifelse(completed == 1, draw, .22),
-         away_win_prob = ifelse(completed == 1, away_win, .78 - home_win_prob))
+teams.import <- read_csv("https://raw.githubusercontent.com/jeff-townsend/sports-analytics/main/data/EPL/epl_elo_2425.csv")
 
-set.seed(811)
-simulations <- 25000
-epl.seasons <- data.frame(id = c(1:(nrow(epl.schedule)*simulations)),
-                          season_id = rep(c(1:simulations), each = nrow(epl.schedule)),
-                          week = epl.schedule$week,
-                          home = epl.schedule$home,
-                          away = epl.schedule$away,
-                          home_win_prob = epl.schedule$home_win_prob,
-                          draw_prob = epl.schedule$draw_prob,
-                          away_win_prob = epl.schedule$away_win_prob)
+teams <-
+  games.import %>%
+  distinct(home) %>%
+  rename(fbref_team = home) %>%
+  arrange(fbref_team)
 
+teams$tsl_team <- teams.import$team
+teams$elo <- teams.import$elo
 
-epl.simulations <-
-  epl.seasons %>%
-  mutate(rng = runif(nrow(epl.seasons))) %>%
-  mutate(home_win = ifelse(rng < home_win_prob, 1, 0),
-         draw = ifelse(rng > (1 - draw_prob), 1, 0),
-         away_win = 1 - home_win - draw,
+games <-
+  games.import %>%
+  inner_join(teams %>%
+               select(fbref_team, elo) %>%
+               rename(team = fbref_team),
+             by = c("home" = "team")) %>%
+  rename(home_elo = elo) %>%
+  inner_join(teams %>%
+               select(fbref_team, elo) %>%
+               rename(team = fbref_team),
+             by = c("away" = "team")) %>%
+  rename(away_elo = elo) %>%
+  mutate(elo_diff = home_elo - away_elo) %>%
+  mutate(home_win_prob = 1 / (10^(-elo_diff/400) + 1) * .78,
+         draw_prob = .22,
+         away_win_prob = .78 - home_win_prob)
+
+set.seed(816)
+simulations <- 10000
+seasons <- data.frame(season_id = c(1:simulations))
+
+season.games <- data.frame(season_game_id = c(1:(nrow(games)*simulations)),
+                           season_id = rep(seasons$season_id, each = nrow(games)),
+                           game_id = games$game_id,
+                           week = games$week,
+                           home = games$home,
+                           away = games$away,
+                           home_win_prob = games$home_win_prob,
+                           draw_prob = games$draw_prob,
+                           away_win_prob = games$away_win_prob)
+  
+game.simulations <-
+  season.games %>%
+  mutate(rng = runif(nrow(season.games)),
+         home_win = ifelse(rng <= home_win_prob, 1, 0),
+         away_win = ifelse(rng >= (1 - away_win_prob), 1, 0),
+         draw = 1 - home_win - away_win,
          home_points = 3*home_win + draw,
          away_points = 3*away_win + draw)
 
 home.performance <-
-  epl.simulations %>%
-  group_by(season_id, home) %>%
-  summarize(points = sum(home_points)) %>%
+  game.simulations %>%
+  group_by(home, season_id) %>%
+  summarize(matches = n(),
+            wins = sum(home_win),
+            draws = sum(draw),
+            losses = sum(away_win),
+            points = sum(home_points)) %>%
   ungroup() %>%
   rename(team = home)
 
 away.performance <-
-  epl.simulations %>%
-  group_by(season_id, away) %>%
-  summarize(points = sum(away_points)) %>%
+  game.simulations %>%
+  group_by(away, season_id) %>%
+  summarize(matches = n(),
+            wins = sum(away_win),
+            draws = sum(draw),
+            losses = sum(home_win),
+            points = sum(away_points)) %>%
   ungroup() %>%
   rename(team = away)
 
-team.performance <-
-  home.performance %>%
-  inner_join(away.performance, by = c("season_id", "team")) %>%
-  mutate(points = points.x + points.y) %>%
-  select(season_id, team, points) %>%
-  group_by(season_id) %>%
-  mutate(placement = rank(desc(points), ties.method = "random")) %>%
-  inner_join(tsl.scoring, by = "placement")
-
-team.summary <-
-  team.performance %>%
-  inner_join(tsl.teams, by = "team") %>%
-  group_by(team, owner, cost) %>%
-  summarize(avg_points = mean(points),
-            avg_tsl_points = mean(tsl_points),
-            league_title = mean(ifelse(placement == 1, 1, 0)),
-            top_4 = mean(ifelse(placement <= 4, 1, 0)),
-            top_10 = mean(ifelse(placement <= 10, 1, 0)),
-            relegation = mean(ifelse(placement >= 18, 1, 0)),
-            avg_tsl_net = mean(tsl_points - cost))
-View(team.summary %>% arrange(desc(avg_tsl_points)))
-
-tsl.summary <-
-  team.performance %>%
-  inner_join(tsl.teams, by = "team") %>%
-  group_by(season_id, owner) %>%
-  summarize(tsl_points = sum(tsl_points),
-            tsl_cost = sum(cost)) %>%
+tables <-
+  rbind(home.performance, away.performance) %>%
+  group_by(team, season_id) %>%
+  summarize(matches = sum(matches),
+            wins = sum(wins),
+            draws = sum(draws),
+            losses = sum(losses),
+            points = sum(points)) %>%
   ungroup() %>%
-  mutate(tsl_net = tsl_points - tsl_cost)
+  group_by(season_id) %>%
+  mutate(placement = rank(desc(points), ties.method = "random"),
+         top4 = ifelse(placement <= 4, 1, 0))
 
-tsl.averages <-
-  tsl.summary %>%
-  group_by(owner) %>%
-  summarize(average = mean(tsl_net))
+summary <-
+  tables %>%
+  group_by(team) %>%
+  summarize(wins = mean(wins),
+            draws = mean(draws),
+            losses = mean(losses),
+            points = mean(points),
+            top4 = mean(top4))
 
-ggplot(tsl.summary, aes(x = tsl_net, fill = owner)) +
-  geom_density(alpha = .75) +
-  geom_vline(xintercept = tsl.averages$average[1], color = "#FF2700") +
-  geom_vline(xintercept = tsl.averages$average[2], color = "#77AB43") +
-  geom_vline(xintercept = tsl.averages$average[3], color = "#008FD5") +
-  theme_fivethirtyeight() +
-  theme(legend.title = element_blank()) +
-  ggtitle("EPL TSL Performance - Matchweek 3")
-tsl.averages
+sum(teams$elo)
