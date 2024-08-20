c3f50cd7b387f24968c5eb14f583bcdb532aac5e
jeff-townsend <106111263+jeff-townsend@users.noreply.github.com>
2024-08-20 18:57:55 -0400

    Update [TSL] EPL Season Simulations.R

--- [TSL] EPL Season Simulations.R
+++ [TSL] EPL Season Simulations.R
@@ -17,79 +17,74 @@ teams <-
   arrange(fbref_team)
 
 teams$tsl_team <- teams.import$team
-teams$rgf <- teams.import$gd / 2
-teams$rga <- teams.import$gd / 2
-teams$rating <- teams.import$gd
-
-hfa <- 0.3
-games <-
-  games.import %>%
-  inner_join(teams %>%
-               select(fbref_team, rgf, rga) %>%
-               rename(team = fbref_team),
-             by = c("home" = "team")) %>%
-  rename(home_rgf = rgf,
-         home_rga = rga) %>%
-  inner_join(teams %>%
-               select(fbref_team, rgf, rga) %>%
-               rename(team = fbref_team),
-             by = c("away" = "team")) %>%
-  rename(away_rgf = rgf,
-         away_rga = rga) %>%
-  mutate(home_lambda = home_rgf - away_rga + 1.5 + hfa/2,
-         away_lambda = away_rgf - home_rga + 1.5 - hfa/2)
+teams$gd <- teams.import$gd
 
 set.seed(816)
-simulations <- 10000
-seasons <- data.frame(season_id = c(1:simulations))
+s <- 10000
 
-season.games <- data.frame(season_game_id = c(1:(nrow(games)*simulations)),
-                           season_id = rep(seasons$season_id, each = nrow(games)),
-                           game_id = games$game_id,
-                           week = games$week,
-                           home = games$home,
-                           away = games$away,
-                           home_lambda = games$home_lambda,
-                           away_lambda = games$away_lambda)
+team.seasons <- data.frame(id = c(1:(nrow(teams)*s)),
+                           season_id = rep(c(1:s), each = nrow(teams)),
+                           team = teams$fbref_team,
+                           team_gd = teams$gd,
+                           season_gd = rnorm(nrow(teams)*s,
+                                             mean = teams$gd,
+                                             sd = 0.28))
+team.seasons$season_rgf = with(team.seasons, 0.5*season_gd)
+team.seasons$season_rga = with(team.seasons, 0.5*season_gd)
 
-game.simulations <-
-  season.games %>%
-  mutate(home_goals = rpois(nrow(season.games), lambda = home_lambda),
-         away_goals = rpois(nrow(season.games), lambda = away_lambda),
+hfa <- 0.3
+games <- data.frame(id = c(1:(nrow(games.import)*s)),
+                    season_id = rep(c(1:s), each = nrow(games.import)),
+                    game_id = c(1:(nrow(games.import))),
+                    home_team = games.import$home,
+                    away_team = games.import$away)
+
+simulations <-
+  games %>%
+  inner_join(team.seasons %>% select(season_id, team, season_rgf, season_rga),
+             by = c("season_id", "home_team" = "team")) %>%
+  rename(home_rgf = season_rgf,
+         home_rga = season_rga) %>%
+  inner_join(team.seasons %>% select(season_id, team, season_rgf, season_rga),
+             by = c("season_id", "away_team" = "team")) %>%
+  rename(away_rgf = season_rgf,
+         away_rga = season_rga) %>%
+  mutate(home_lambda = home_rgf - away_rga + 1.5 + 0.5*hfa,
+         away_lambda = away_rgf - home_rga + 1.5 - 0.5*hfa) %>%
+  mutate(home_goals = rpois(nrow(games), lambda = ifelse(home_lambda < 0, 0, home_lambda)),
+         away_goals = rpois(nrow(games), lambda = ifelse(away_lambda < 0, 0, away_lambda)),
          home_win = ifelse(home_goals > away_goals, 1, 0),
          away_win = ifelse(away_goals > home_goals, 1, 0),
          draw = ifelse(home_goals == away_goals, 1, 0),
          home_points = 3*home_win + draw,
          away_points = 3*away_win + draw)
 
-home.performance <-
-  game.simulations %>%
-  group_by(home, season_id) %>%
-  summarize(matches = n(),
-            wins = sum(home_win),
-            gf = sum(home_goals),
-            ga = sum(away_goals),
-            draws = sum(draw),
-            losses = sum(away_win),
-            points = sum(home_points)) %>%
-  ungroup() %>%
-  rename(team = home)
-
-away.performance <-
-  game.simulations %>%
-  group_by(away, season_id) %>%
-  summarize(matches = n(),
-            wins = sum(away_win),
-            gf = sum(away_goals),
-            ga = sum(home_goals),
-            draws = sum(draw),
-            losses = sum(home_win),
-            points = sum(away_points)) %>%
-  ungroup() %>%
-  rename(team = away)
+performance <-
+  rbind(simulations %>%
+        group_by(home_team, season_id) %>%
+        summarize(matches = n(),
+                  wins = sum(home_win),
+                  gf = sum(home_goals),
+                  ga = sum(away_goals),
+                  draws = sum(draw),
+                  losses = sum(away_win),
+                  points = sum(home_points)) %>%
+        ungroup() %>%
+        rename(team = home_team),
+        simulations %>%
+        group_by(away_team, season_id) %>%
+        summarize(matches = n(),
+                  wins = sum(away_win),
+                  gf = sum(away_goals),
+                  ga = sum(home_goals),
+                  draws = sum(draw),
+                  losses = sum(home_win),
+                  points = sum(away_points)) %>%
+        ungroup() %>%
+        rename(team = away_team))
 
 tables <-
-  rbind(home.performance, away.performance) %>%
+  performance %>%
   group_by(team, season_id) %>%
   summarize(matches = sum(matches),
             wins = sum(wins),
@@ -102,10 +97,9 @@ tables <-
   group_by(season_id) %>%
   mutate(placement = rank(desc(points), ties.method = "random"),
          title = ifelse(placement == 1, 1, 0),
-         top2 = ifelse(placement <= 2, 1, 0),
          top4 = ifelse(placement <= 4, 1, 0),
-         top6 = ifelse(placement <= 6, 1, 0),
-         top10 = ifelse(placement <= 10, 1, 0))
+         top10 = ifelse(placement <= 10, 1, 0),
+         relegation = ifelse(placement >= 18, 1, 0))
 
 summary <-
   tables %>%
@@ -118,10 +112,16 @@ summary <-
             losses = mean(losses),
             gd = mean(gf - ga) / 38,
             title = mean(title),
-            top2 = mean(top2),
             top4 = mean(top4),
-            top6 = mean(top6),
-            top10 = mean(top10))
+            top10 = mean(top10),
+            relegation = mean(relegation))
+
+placements <-
+  tables %>%
+  group_by(team, placement) %>%
+  summarize(frequency = n()) %>%
+  ungroup() %>%
+  mutate(proportion = frequency / s)
 
 # win.probs <-
 #   game.simulations %>%
