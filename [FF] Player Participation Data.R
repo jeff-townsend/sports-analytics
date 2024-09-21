1d9078bd3b14e2ab6b9c05e4847cefa17efaf08d
jeff-townsend <106111263+jeff-townsend@users.noreply.github.com>
2024-09-21 14:01:06 -0400

    Update [FF] Player Participation Data.R

--- [FF] Player Participation Data.R
+++ [FF] Player Participation Data.R
@@ -6,8 +6,12 @@ library(nflreadr)
 pbp <-
   load_pbp(2023) %>%
   filter(season_type == "REG",
-         play_type %in% c("run", "pass", "qb_kneel"))
-pbp$pbp_id <- c(1:(nrow(pbp)))
+         play_type %in% c("run", "pass"),
+         two_point_attempt == 0) %>%
+  select(game_id, season, week, play_id, desc, play_type,
+         down, ydstogo, yardline_100, goal_to_go, qtr, score_differential, xpass,
+         yards_gained, epa, passer_id, rusher_id, receiver_id)
+pbp$id <- c(1:(nrow(pbp)))
 
 # fantasy-relevant players
 
@@ -15,7 +19,9 @@ rosters <-
   load_rosters(2023) %>%
   filter(position %in% c("QB", "RB", "WR", "TE"),
          !is.na(gsis_id)) %>%
-  rename(player = full_name)
+  rename(player_id = gsis_id,
+         player = full_name) %>%
+  select(player_id, season, player, position)
 
 # game participation data
 
@@ -23,88 +29,42 @@ participation <-
   load_participation(2023) %>%
   rename(game_id = nflverse_game_id,
          team = possession_team) %>%
-  inner_join(pbp %>% select(season, game_id, play_id), by = c("game_id", "play_id")) %>%
-  mutate(rbs = substr(offense_personnel,
-                      unlist(gregexpr("RB", offense_personnel)) - 2,
-                      unlist(gregexpr("RB", offense_personnel)) - 2),
-         tes = substr(offense_personnel,
-                      unlist(gregexpr("TE", offense_personnel)) - 2,
-                      unlist(gregexpr("TE", offense_personnel)) - 2),
-         wrs = substr(offense_personnel,
-                      unlist(gregexpr("WR", offense_personnel)) - 2,
-                      unlist(gregexpr("WR", offense_personnel)) - 2))
-participation$participation_id <- c(1:(nrow(participation)))
+  select(game_id, play_id, team, offense_players) %>%
+  inner_join(pbp, by = c("game_id", "play_id"))
 
 players <-
   participation %>%
   separate_rows(offense_players, sep = ";") %>%
   rename(player_id = offense_players) %>%
-  select(participation_id, season, game_id, player_id, team, offense_personnel, rbs, tes, wrs) %>%
-  inner_join(rosters %>% select(season, gsis_id, player, position), by = c("season", "player_id" = "gsis_id"))
+  select(id, season, week, game_id, play_id, player_id, team, passer_id, rusher_id, receiver_id) %>%
+  inner_join(rosters %>% select(gsis_id, player, position), by = "player_id") %>%
+  mutate(used = ifelse(player_id == passer_id | player_id == rusher_id | player_id == receiver_id, 1, 0)) %>%
+  mutate(used = ifelse(is.na(used), 0, used))
 
 team.games <-
   participation %>%
-  group_by(game_id, team) %>%
-  summarize(team_snaps = n(),
-            team_snaps_11 = sum(ifelse(offense_personnel == "1 RB, 1 TE, 3 WR", 1, 0)),
-            team_snaps_12 = sum(ifelse(offense_personnel == "1 RB, 2 TE, 2 WR", 1, 0)),
-            team_snaps_13 = sum(ifelse(offense_personnel == "1 RB, 3 TE, 1 WR", 1, 0)),
-            team_snaps_21 = sum(ifelse(offense_personnel == "2 RB, 1 TE, 2 WR", 1, 0)),
-            team_snaps_22 = sum(ifelse(offense_personnel == "2 RB, 2 TE, 1 WR", 1, 0))) %>%
-  ungroup() %>%
-  mutate(team_snaps_other = team_snaps - team_snaps_11 - team_snaps_12 - team_snaps_13 - team_snaps_21 - team_snaps_22)
+  group_by(game_id, season, team) %>%
+  summarize(team_snaps = n())
 
 player.games <-
   players %>%
-  group_by(player_id, player, position, game_id, team) %>%
+  group_by(player_id, game_id, season, week, player, position, team) %>%
   summarize(snaps = n(),
-            snaps_11 = sum(ifelse(offense_personnel == "1 RB, 1 TE, 3 WR", 1, 0)),
-            snaps_12 = sum(ifelse(offense_personnel == "1 RB, 2 TE, 2 WR", 1, 0)),
-            snaps_13 = sum(ifelse(offense_personnel == "1 RB, 3 TE, 1 WR", 1, 0)),
-            snaps_21 = sum(ifelse(offense_personnel == "2 RB, 1 TE, 2 WR", 1, 0)),
-            snaps_22 = sum(ifelse(offense_personnel == "2 RB, 2 TE, 1 WR", 1, 0))) %>%
+            opportunities = sum(used)) %>%
   ungroup() %>%
-  mutate(snaps_other = snaps - (snaps_11 + snaps_12 + snaps_13 + snaps_21 + snaps_22))
-  
-player.snaps <-
+  inner_join(team.games, by = c("season", "game_id", "team")) %>%
+  mutate(snap_rate = snaps / team_snaps,
+         usage_rate = opportunities / snaps)
+
+player.seasons <-
   player.games %>%
-  inner_join(team.games, by = c("game_id", "team")) %>%
-  group_by(player_id, player, position) %>%
-  summarize(snaps = sum(snaps),
-            snaps_11 = sum(snaps_11),
-            snaps_12 = sum(snaps_12),
-            snaps_13 = sum(snaps_13),
-            snaps_21 = sum(snaps_21),
-            snaps_22 = sum(snaps_22),
-            snaps_other = sum(snaps_other),
-            team_snaps = sum(team_snaps),
-            team_snaps_11 = sum(team_snaps_11),
-            team_snaps_12 = sum(team_snaps_12),
-            team_snaps_13 = sum(team_snaps_13),
-            team_snaps_21 = sum(team_snaps_21),
-            team_snaps_22 = sum(team_snaps_22),
-            team_snaps_other = sum(team_snaps_other)) %>%
+  group_by(player_id, season, player, position, team) %>%
+  summarize(gp = n(),
+            snaps = sum(snaps) / n(),
+            opportunities = sum(opportunities) / n(),
+            team_snaps = sum(team_snaps) / n()) %>%
   ungroup() %>%
-  mutate(snap_share = snaps / team_snaps,
-         snap_share_11 = snaps_11 / team_snaps_11,
-         snap_share_12 = snaps_12 / team_snaps_12,
-         snap_share_13 = snaps_13 / team_snaps_13,
-         snap_share_21 = snaps_21 / team_snaps_21,
-         snap_share_22 = snaps_22 / team_snaps_22,
-         snap_share_other = snaps_other / team_snaps_other)
-  #select(-c(team_snaps, team_snaps_11, team_snaps_12, team_snaps_13, team_snaps_21, team_snaps_22, team_snaps_other))
-  
+  mutate(snap_rate = snaps / team_snaps,
+         usage_rate = opportunities / snaps)
 
-## WR snaps by personnel
-
-wr.snaps <-
-  player.snaps %>%
-  filter(position == "WR") %>%
-  mutate(snaps_2wr = snaps_12 + snaps_21,
-         snaps_3wr = snaps_11,
-         team_snaps_2wr = team_snaps_12 + team_snaps_21,
-         team_snaps_3wr = team_snaps_11,
-         snap_share_2wr = snaps_2wr / team_snaps_2wr,
-         snap_share_3wr = snaps_3wr / team_snaps_3wr,
-         personnel_dependency = snap_share_3wr - snap_share_2wr) %>%
-  select(player, snaps, snaps_2wr, snaps_3wr, snap_share_2wr, snap_share_3wr, personnel_dependency)
+View(player.seasons %>% filter(position == "RB"))
