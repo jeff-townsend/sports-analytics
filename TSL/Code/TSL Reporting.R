6d5b858cd3146f7e16bfd608cade0e282605e1fe
jeff-townsend <106111263+jeff-townsend@users.noreply.github.com>
2026-05-16 11:55:20 -0400

    Update TSL Reporting.R

--- TSL/Code/TSL Reporting.R
+++ TSL/Code/TSL Reporting.R
@@ -8,3 +8,34 @@ draft.picks <- read_csv("https://raw.githubusercontent.com/jeff-townsend/sports-
 keepers <- read_csv("https://raw.githubusercontent.com/jeff-townsend/sports-analytics/main/TSL/Data/keepers.csv")
 trades <- read_csv("https://raw.githubusercontent.com/jeff-townsend/sports-analytics/main/TSL/Data/trades.csv")
 team.results <- read_csv("https://raw.githubusercontent.com/jeff-townsend/sports-analytics/main/TSL/Data/team_results.csv")
+
+drafted.teams <-
+  draft.picks %>%
+  inner_join(teams %>% select(season_id, team, franchise), by = c("season_id", "team")) %>%
+  inner_join(seasons %>% select(season_id, season), by = "season_id") %>%
+  mutate(draft_season = season) %>%
+  inner_join(draft.costs, by = "round") %>%
+  rename(team_cost = draft_cost) %>%
+  select(season, owner, team, franchise, draft_season, team_cost)
+
+kept.teams <-
+  keepers %>%
+  inner_join(teams %>% select(season_id, team, franchise), by = c("season_id", "team")) %>%
+  inner_join(seasons %>% select(season_id, season), by = "season_id") %>%
+  inner_join(drafted.teams %>% select(franchise, draft_season, team_cost), by = "franchise") %>%
+  rename(draft_cost = team_cost) %>%
+  filter(draft_season < season) %>%
+  group_by(owner, franchise, season) %>%
+  mutate(max_draft_season = max(draft_season)) %>%
+  filter(draft_season == max_draft_season) %>%
+  mutate(team_cost = draft_cost + 5 * (season - draft_season - 1)) %>%
+  select(season, owner, team, franchise, draft_season, team_cost)
+
+rosters <-
+  rbind(drafted.teams, kept.teams) %>%
+  left_join(trades %>%
+              inner_join(seasons %>% select(season_id, season), by = "season_id") %>%
+              rename(owner = from_owner),
+            by = c("season", "owner", "team")) %>%
+  mutate(owner = ifelse(is.na(trade_id), owner, to_owner)) %>%
+  select(season, owner, team, draft_season, team_cost)
