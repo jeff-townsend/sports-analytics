f67957e493307a839057c542bba0342c944dd868
jeff-townsend <106111263+jeff-townsend@users.noreply.github.com>
2026-06-04 16:36:40 -0400

    Update TSL Reporting.R

--- TSL/Code/TSL Reporting.R
+++ TSL/Code/TSL Reporting.R
@@ -1,5 +1,6 @@
 library(readr)
 library(tidyverse)
+library(ggthemes)
 
 seasons <- read_csv("https://raw.githubusercontent.com/jeff-townsend/sports-analytics/main/TSL/Data/seasons.csv")
 teams <- read_csv("https://raw.githubusercontent.com/jeff-townsend/sports-analytics/main/TSL/Data/teams.csv")
@@ -11,7 +12,7 @@ team.results <- read_csv("https://raw.githubusercontent.com/jeff-townsend/sports
 
 drafted.teams <-
   draft.picks %>%
-  inner_join(teams %>% select(season_id, team, franchise),
+  inner_join(teams %>% select(season_id, team, franchise, league),
              by = c("season_id", "team")) %>%
   inner_join(seasons %>% select(season_id, season),
              by = "season_id") %>%
@@ -19,11 +20,11 @@ drafted.teams <-
   inner_join(draft.costs,
              by = "round") %>%
   rename(team_cost = draft_cost) %>%
-  select(season_id, season, owner, team, franchise, draft_season, team_cost)
+  select(season_id, season, owner, team, franchise, league, draft_season, team_cost)
 
 kept.teams <-
   keepers %>%
-  inner_join(teams %>% select(season_id, team, franchise),
+  inner_join(teams %>% select(season_id, team, franchise, league),
              by = c("season_id", "team")) %>%
   inner_join(seasons %>% select(season_id, season),
              by = "season_id") %>%
@@ -35,22 +36,67 @@ kept.teams <-
   mutate(max_draft_season = max(draft_season)) %>%
   filter(draft_season == max_draft_season) %>%
   mutate(team_cost = draft_cost + 5 * (season - draft_season - 1)) %>%
-  select(season_id, season, owner, team, franchise, draft_season, team_cost)
+  select(season_id, season, owner, team, franchise, league, draft_season, team_cost)
 
 rosters <-
   rbind(drafted.teams, kept.teams) %>%
   left_join(trades %>% rename(owner = from_owner),
             by = c("season_id", "owner", "team")) %>%
   mutate(owner = ifelse(is.na(trade_id), owner, to_owner)) %>%
-  select(season_id, season, owner, team, draft_season, team_cost)
+  select(season_id, season, owner, team, league, draft_season, team_cost)
 
-standings <-
+## Performance
+
+team.performance <-
   rosters %>%
   inner_join(team.results %>% select(season_id, team, points),
              by = c("season_id", "team")) %>%
+  mutate(net_points = points - team_cost)
+
+## Historical Standings
+
+standings <-
+  team.performance %>%
   group_by(season, owner) %>%
   summarize(total_points = sum(points),
             total_cost = sum(team_cost)) %>%
   ungroup() %>%
   mutate(net_points = total_points - total_cost) %>%
   arrange(season, desc(net_points))
+
+## Performance by League
+
+league.performance <-
+  team.performance %>%
+  group_by(season, owner, league) %>%
+  summarize(teams = n(),
+            total_points = sum(points),
+            total_cost = sum(team_cost)) %>%
+  ungroup() %>%
+  mutate(net_points = total_points - total_cost,
+         net_points_avg = net_points / teams) %>%
+  arrange(season, desc(net_points))
+
+
+## Charts
+
+ggplot(team.performance %>%
+         group_by(owner, league) %>%
+         summarize(team_cost = mean(team_cost)) %>%
+         arrange(league, owner),
+       aes(x = owner, fill = league, y = team_cost)) +
+  geom_bar(stat = "identity", position = position_dodge()) +
+  theme_fivethirtyeight() +
+  scale_fill_manual(values = c("#219ebc", "#023047", "#ffb703", "#fb8500")) +
+  theme(legend.title = element_blank()) +
+  ggtitle("Average Team Cost by League")
+
+team.performance %>%
+  group_by(league) %>%
+  summarize(net_points = mean(net_points)) %>%
+  arrange(league)
+
+team.performance %>%
+  group_by(owner) %>%
+  summarize(team_cost = mean(team_cost)) %>%
+  arrange(owner)
